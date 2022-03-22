74941bacaac01c8d24de2458d78ff10e151fd795
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-03-22 14:41:37 +0100

    styling main content

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function Main(){
     return(
-        <h1>Main</h1>
+        <main>
+            <h1 className="main--title">Quick Facts About Kosovo</h1>
+            <ul className="main--facts">
+                <li>Kosovo is the second-youngest country in the world, declaring its on <b>February 17, 2008</b></li>   
+                <li>With a median age of 29.1 years, Kosovo has among the youngest populations in Europe. More than 40 percent of the population is under 25.</li>   
+                <li>More than 110 countries recognize Kosovo as an independent country. However, a bunch of countries including Serbia, Russia, and some EU countries still do not recognize Kosovo as an independent country for that matter.</li>   
+                <li>Kosovo (10,887 sq km) is the smallest country in the Balkans and it is about the size of Jamaica (10,991 sq km) or Lebanon (10,400 sq km). Kosovo is slightly larger than Delaware.</li>   
+                <li>n 2009, Kosovo joined the IMF and World Bank</li>   
+                <li><b>Bill Clinton</b>, the former president of the United States visited Kosovo on November 23, 1999. Interestingly, there is a statue of Bill Clinton in Kosovo.</li>
+            </ul>
+        </main>
     )
 }
 
